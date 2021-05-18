c4c60b66554ecc143c1cebe55598d8c1b86ec36d
SaraGimenoOrtiz <83558404+SaraGimenoOrtiz@users.noreply.github.com>
2021-05-18 10:38:32 +0200

    Add files via upload

--- HL_Class_3.py
+++ HL_Class_3.py
@@ -16,13 +16,30 @@ my_file.close()
 #TASK 2
 
 def change_mark(inp_mark):
-    output=inp_mark/10
+    if 80<=inp_mark<=100:
+        output="A"
+    elif 70<=inp_mark<=79:
+        output="B"
+    elif 60<=inp_mark<=69:
+        output="C"
+    elif 50<=inp_mark<=59:
+        output="D"
+    elif 40<=inp_mark<=49:
+        output="E"
+    elif 30<=inp_mark<=39:
+        output="F"
+    elif 20<=inp_mark<=29:
+        output="FF"
+    elif 10<=inp_mark<=19:
+        output="G"
+    else:
+        output="H" 
     print(output)
     return output
-mark=int(input("Introduce your mark of the exam in %"))
+mark=int(input("Introduce your mark of the exam in %:"))
 change_mark(mark)
 
-goal=int(input("Introduce your mark target in %"))
+goal=int(input("Introduce your mark target in %:"))
 change_mark(goal)
 
 if mark<goal:
